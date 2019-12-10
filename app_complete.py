4a2c4405e41d9a8be5d040358ce67ebbd9277129
Jakub Ratajík <jakub.ratajik@gmail.com>
2019-12-10 11:13:20 +0100

    Update app_complete.py

--- app_complete.py
+++ app_complete.py
@@ -1,14 +1,4 @@
 '''
-GALINSKI:
-DONEzladit dizajn s ostatnymi
-DONE radiobutton - nech je jasne, ked je kliknute
-DONEvydavatel, typ karty, limit (rodelit pravu stranu, teda ze sa nevybera zo 4 moznosti, ale dvoch a dvoch)
-DONE farba pisma nech je v entry citatelna
-DONE tlacidlo na odhlasenie sa
-DONEdat lavej strane vzduch, oddialit info
-DONErollbox vyber karty dat hore
-DONE Mato, bolo by podla mna super, ked budes mat cas, ze by si vsetky pozicie co tu su zmazal a spravil ich nanovo a vsetky zavisle, teda relativne podla w a h - nie ako teraz, ze niektore maju suradnice 100 a ked zmenime v skole velkost frameu, tak sa to cele rozbije
-
 miesto rollboxu na klienta spravit search engine
 miesto 2 tlacidiel odblokovat a zablokovat spravit jedno, ktore bude menit stav
 pridat do comboboxu klienti rodne cisla
@@ -63,7 +53,13 @@ c.grid(sticky='s')
 ########## variables
 
 users = {'kubo': 'ok', 'mato':'matojefrajer'} ##mena a hesla na prihlasovanie -- neskor by bolo dobre aby sme to dali do nejakej databazy v subore alebo co
-##users.update({'' : ''}) ## toto vzdy odkomentuj aby si nemusel stale pri spustani zadavat login
+users.update({'' : ''}) ## toto vzdy odkomentuj aby si nemusel stale pri spustani zadavat login
+clients = {'jano':'mrkva', 'fero' :'dreveny', 'jano':'stolny'}
+foundClients = []
+
+
+currentClient = 'Maros Klamar'
+
 
 lineCislo_karty = incorrectNameOrPassword = lineClientName = lineDatum_vytvorenia = timeShow = lineDatum_platnosti = lineDlzna_suma = lineBlokovana = incorrectNameOrPassword = ''
 
@@ -76,7 +72,7 @@ imageDebet = tk.PhotoImage(file = 'obrazky/debet.png')
 imageKredit = tk.PhotoImage(file = 'obrazky/kredit.png')
 imageLogoBanky = tk.PhotoImage(file = 'obrazky/logobanky.png')
 
-clients = ['--- vyberte klienta ---','Jano', 'Fero', 'Dominik']
+##clients = ['--- vyberte klienta ---','Jano', 'Fero', 'Dominik']
 cardsList = ['--- vyberte kartu ---', 'SK506065320', 'SK35408540635', 'SK0468785343', 'and more...', 'SK506065320', 'SK35408540635', 'SK0468785343', 'and more...']
 
 clientName = 'Maros Klamar'
@@ -93,7 +89,6 @@ blokovana = '0'
 def essentialLook():
     mainBorder = c.create_rectangle(widthLines//2, widthLines//2, w-widthLines//2, h-widthLines//2, outline = colorElement,width = widthLines)
     horBorder = c.create_line(0, borders*5, w, borders*5, fill = colorElement, width = widthLines) #before: y = h//borders*3
-    vertLine = c.create_line(w//2, borders*5, w//2, h, width = widthLines, fill = colorElement)
 
 def timeNow():
     global timeShow
@@ -112,7 +107,7 @@ def loginAuthentication():
         c.destroy()
         c = tk.Canvas(width = w, height = h, bg = backgroundColor, cursor = 'arrow')
         c.pack()
-        application()
+        chooseClientScreen()
     else:
         c.delete(incorrectNameOrPassword)
         incorrectNameOrPassword = c.create_text(w//4*3-borders, h//10*8-borders, text = 'nesprávne meno alebo heslo!', fill = colorElement, font = fontMain + (fontSizeBig,) + (fontItalic,))
@@ -120,18 +115,25 @@ def loginAuthentication():
 def loginScreen():
     global entryName, entryPassword
     essentialLook()
+    vertLine = c.create_line(w//2, borders*5, w//2, h, width = widthLines, fill = colorElement)
+
+    
     c.create_text(w - borders, borders*2 , anchor = 'se', text = 'verzia: 84.6.2', fill=colorElement,font = fontMain + (fontSizeSmall,) + (fontItalic,))
 
     timeNow()
     c.create_text(w//2 + borders*3 - widthLines/2, h//10*4, anchor = 'w',text = 'MENO', fill=colorElement, font = fontMain + (fontSizeBig,) + (fontItalic,))
-    entryName = tk.Entry(master = c, font = fontWidget + (fontSizeBig,) + (fontStyleNone,), foreground = colorElement,insertbackground=colorElement)
+    entryName = tk.Entry(c, font = fontWidget + (fontSizeBig,) + (fontStyleNone,), foreground = colorElement,insertbackground=colorElement)
     entryName.pack()
     entryName.place(x = w//2 + borders*2 - widthLines/2, y = h//10*4 + int(fontSizeBig)/2*3, anchor='w')
+##    entryName.bind("<Return>", loginAuthentication)
 
+##    entryName.insert(0,'meno',font='Arial')
+    
     c.create_text(w//2 + borders*3 - widthLines/2, h//10*6, anchor = 'w', text = 'HESLO', fill = colorElement,font = fontMain + (fontSizeBig,) + (fontItalic,))
-    entryPassword = tk.Entry(master = c, font = fontWidget + (fontSizeBig,) + (fontStyleNone,), foreground = colorElement,insertbackground=colorElement, show = '*')
+    entryPassword = tk.Entry(c, font = fontWidget + (fontSizeBig,) + (fontStyleNone,), foreground = colorElement,insertbackground=colorElement, show = '*')
     entryPassword.pack()
     entryPassword.place(x = w//2 + borders*2 - widthLines/2, y = h//10*6 + int(fontSizeBig)/2*3, anchor='w')
+##    entryPassword.bind("<Return>", loginAuthentication)
 
     logoBanky = tk.Label(master = c, image = imageLogoBanky, bg = backgroundColor)
     logoBanky.pack()
@@ -141,15 +143,38 @@ def loginScreen():
     buttonLogin.pack()
     buttonLogin.place(x = w-borders*2, y = h-borders*2, anchor = 'se')
 
+def chooseClientScreen():
+    global searchEngineEntry,c
+
+    essentialLook()
+    c.create_text(w - borders, borders*2 , anchor = 'se', text = 'verzia: 84.6.2', fill=colorElement,font = fontMain + (fontSizeSmall,) + (fontItalic,))
+    c.create_text(w/10*3+borders, h/4, text = 'vyhľadanie klienta', font = fontMain + (fontSizeBig,) + (fontStyleNone,),fill=colorElement,anchor='e')
+    timeNow()
+    
+    searchEngineEntry = tk.Entry(font = fontWidget + (fontSizeMedium,) + (fontStyleNone,), foreground = colorElement,insertbackground=colorElement)
+    searchEngineEntry.pack()
+    searchEngineEntry.place(x = w/5*3, y = h/4, anchor='e')
+
+    searchEngineButton = tk.Button(command = searchClient, width = 15, bg=colorElement,activebackground = colorElement,foreground = backgroundColor,text = 'hľadať',cursor='hand2',font = fontWidget + (fontSizeMedium,) + (fontBold,))
+    searchEngineButton.pack()
+    searchEngineButton.place(x = w/5*4+borders, y = h/4, anchor='e')
+
+    comboClients = ttk.Combobox(font = fontWidget + (fontSizeSmall,) + (fontStyleNone,), values = foundClients, width = 30, state='readonly', justify = 'center')
+    comboClients.pack()
+    comboClients.place(x = w/2, y = h/2-borders*3, anchor='c')
+
 
 
 def application():
-    global comboCards
-    
-    c.delete('all')
+    global comboCards,searchEngineEntry,c
+    c.destroy()
+    c = tk.Canvas(width = w, height = h, bg = backgroundColor, cursor = 'arrow')
+    c.pack()
     essentialLook()
+    vertLine = c.create_line(w//2, borders*5, w//2, h, width = widthLines, fill = colorElement)
+
     
-    headline1 = c.create_text(borders*2, (borders*5+widthLines/2)/2,text='Aktuálne pracujete s klientom:', anchor = 'w', fill=colorElement,font = fontMain + (fontSizeMedium,) + (fontItalic,))
+    headline1 = c.create_text(borders*2, (borders*5+widthLines/2)/2,text= f'Aktuálne pracujete s klientom: {currentClient}', anchor = 'w', fill=colorElement,font = fontMain + (fontSizeMedium,) + (fontItalic,))
     headline2 = c.create_text(w//4, h//5, text='KARTY KLIENTA', anchor = 'center', fill=colorElement,font = fontMain + (fontSizeMedium,) + (fontBold,))
     headline3 = c.create_text(w//4*3, h//5, text='VYTVORENIE NOVEJ KARTY', anchor = 'center', fill=colorElement,font = fontMain + (fontSizeMedium,) + (fontBold,))
     headline4 = c.create_text(w//8*5 + widthLines, h//2 + borders*10, text='debetná karta', font = fontMain + (fontSizeSmall,) + (fontStyleNone,), fill=colorElement, anchor = 'n')
@@ -159,10 +184,10 @@ def application():
     headline8 = c.create_text(w//2 + borders*2 - widthLines/2, h//2 + borders, text = 'Typ', font = fontMain + (fontSizeSmall,) + (fontStyleNone,),fill=colorElement,anchor='w')
 
 
-    comboUcet = ttk.Combobox(font = fontWidget + (fontSizeSmall,) + (fontStyleNone,), values = clients, width = 30, state='readonly', justify = 'center')
-    comboUcet.current(0) ##ktore sa ukaze na zaciatku ako default
-    comboUcet.pack()
-    comboUcet.place(x = w//2, y = (borders*5+widthLines/2)/2, anchor='c')
+##    comboUcet = ttk.Combobox(font = fontWidget + (fontSizeSmall,) + (fontStyleNone,), values = cardsList, width = 30, state='readonly', justify = 'center')
+##    comboUcet.current(0) ##ktore sa ukaze na zaciatku ako default
+##    comboUcet.pack()
+##    comboUcet.place(x = w//2, y = (borders*5+widthLines/2)/2, anchor='c')
 
     radioButtonVisa = tk.Radiobutton(indicatoron='false',selectcolor=colorElement,highlightthickness=20,activebackground=colorElement,bg = backgroundColor, cursor='hand2',image = imageVisa,variable = visaMastercard,value = 1)
     radioButtonVisa.pack()
@@ -208,6 +233,10 @@ def application():
     logoutButton.pack()
     logoutButton.place(x = w-borders*2, y = (borders*5+widthLines/2)/2, anchor='e')
 
+    changeClientButton = tk.Button(command = changeClient, width = 15, bg=colorElement,activebackground = colorElement,foreground = backgroundColor,text = 'zmeniť klienta',cursor='hand2',font = fontWidget + (fontSizeMedium,) + (fontBold,))
+    changeClientButton.pack()
+    changeClientButton.place(x = w-borders*14, y = (borders*5+widthLines/2)/2, anchor='e')
+
 def displayCard(cislo_karty):
     global clientName, vydavatel, datum_platnosti, id_uctu, dlzna_suma, blokovana, datum_vytvorenia, lineCislo_karty, lineClientName, lineDatum_vytvorenia, lineDatum_platnosti, lineDlzna_suma, lineBlokovana
     c.delete(lineCislo_karty, lineClientName, lineDatum_vytvorenia, lineDatum_platnosti, lineDlzna_suma, lineBlokovana)
@@ -222,7 +251,7 @@ def displayCard(cislo_karty):
     elif blokovana == '1':
         lineBlokovana =    c.create_text(borders*2, h//3 + borders*11, text='Stav: blokovaná', font = fontMain + (fontSizeSmall,) + (fontStyleNone,), fill=colorElement, anchor = 'w')
 
-def chosenCard(f):
+def chosenCard(useless):
     ##bez toho argumentu to nefunguje.. kvoli tomu ze sa to vola v c.bind  a tam je vzdy dany pozicny argument
     
     displayCard(cardsList[comboCards.current()])
@@ -241,10 +270,30 @@ def logout():
     c.pack()
     loginScreen()
 
+def changeClient():
+    c.destroy()
+    c = tk.Canvas(width = w, height = h, bg = backgroundColor, cursor = 'arrow')
+    c.pack()
+    chooseClientScreen()
+
 def read():
     fileKarty = open('karty.txt', 'a')
     fileUcty = open('ucty.txt', 'a')
 
+def handleReturn(event):
+    print("return: event.widget is",event.widget)
+    print("focus is:",c.focus_get())
+
+def searchClient():
+    global foundClients
+    foundClients = []
+    print(clients.items())
+    searchName = searchEngineEntry.get()
+    for key, value in clients.items():
+##        print(f'{key} {value}')
+        if key == searchName or value == searchName:
+            foundClients.append(f'{key} {value}')
+    
     
 
 
@@ -252,6 +301,7 @@ def read():
 
 
 loginScreen()
+
 
 
 
